8414a04b6465c804544c018371cf9e8d9a4337bd
andrewmiculka <31459000+andrewmiculka@users.noreply.github.com>
2019-09-11 15:44:18 -0500

    began logic structure
    set up checks and travels to bank and chickenPen

--- Main.java
+++ Main.java
@@ -1,14 +1,17 @@
 import java.awt.Graphics2D;
 
-import org.osbot.rs07.api.map.constants.Banks;
+//import org.osbot.rs07.api.map.constants.Banks;
 import org.osbot.rs07.script.Script;
 import org.osbot.rs07.script.ScriptManifest;
 import org.osbot.rs07.api.model.Entity;
+import org.osbot.rs07.api.map.*;
+import org.osbot.rs07.api.map.constants.Banks;
 
-@ScriptManifest(author = "Andrew Miculka", info = "Bank search", name = "Bank Test", version = 0, logo = "")
+@ScriptManifest(author = "Andrew Miculka", info = "Harvest chicken bones for sale", name = "Bone Reaper", version = 0, logo = "")
 public class Main extends Script {
 
 	Entity closestBank = null;
+	static Area chickenPen = new Area(new Position(3171, 3289, 0), new Position(3183, 3301, 0));
 	
 	@Override
 	public void onStart() {
@@ -17,11 +20,30 @@ public class Main extends Script {
 
 	@Override
 	public int onLoop() throws InterruptedException {
-		if(getWalking().webWalk(Banks.AL_KHARID, Banks.EDGEVILLE, Banks.FALADOR_EAST, Banks.FALADOR_WEST, Banks.LUMBRIDGE_UPPER)) {
-			log("At the bank . . .");
-			closestBank = getBank().closest();
-			if(closestBank != null && !getBank().isOpen()) {
-				getBank().open();
+		
+		if(getInventory().getEmptySlots() == 0) {
+			log("<<< Inventory Full >>>");
+			//Turn on run
+			//Go to bank
+			if(getWalking().webWalk(Banks.LUMBRIDGE_UPPER, Banks.DRAYNOR)) {
+				log("<<< At The Bank >>>");
+				closestBank = getBank().closest();
+				if(closestBank != null && !getBank().isOpen()) {
+					getBank().open();
+				}
+				else if(getBank().isOpen()) {
+					//deposit bones
+					//check bone amount
+				}
+			}
+		}
+		else {
+			if(getWalking().webWalk(chickenPen)) {
+				//Turn off run
+				//Attack chickens
+			}
+			else {
+				
 			}
 		}
 		return random(200, 300);
